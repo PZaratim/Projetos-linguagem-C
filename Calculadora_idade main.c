4772bd59b4fcac2d90f3839f227d5785dbec698c
Pedro H. Zaratim <zaratimpedro1@gmail.com>
2026-05-05 08:35:01 -0300

    Update Calculadora_idade main.c

--- Calculadora_idade main.c
+++ Calculadora_idade main.c
@@ -7,14 +7,14 @@ void app_main() {
     int data_nascimento = 0;
     const int ano_atual = 2026;
 
-    // 1. Lendo o Nome
+    // Lendo o Nome
     printf("Digite seu nome: \n");
     // Espera até que algo seja digitado no console
     while (scanf("%s", nome) < 1) {
         vTaskDelay(10 / portTICK_PERIOD_MS); 
     }
 
-    // 2. Lendo o Ano
+    // Lendo o Ano
     printf("Digite seu ano de nascimento: \n");
     // Espera até que um número seja digitado
     while (scanf("%d", &data_nascimento) < 1) {
